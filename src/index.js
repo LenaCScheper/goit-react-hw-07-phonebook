2ea89f7e19195ab3f7f56f460f5191369435d647
LenaCScheper <ven.lenysja@ukr.net>
2022-05-04 20:15:01 +0300

    пересаписала скрипт

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import { store } from './redux/store';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+// import { PersistGate } from 'redux-persist/integration/react';
+import 'modern-normalize/modern-normalize.css';
 import './index.css';
+import App from './components/App';
+import * as store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+    <Provider store={store.store}>
+      {/* <PersistGate loading={null} persistor={store.persistor}> */}
+      <App />
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-reportWebVitals();
